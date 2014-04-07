36a01382e54754793528301e4f19ad9c4dbcc980
Sébastien Lavoie <github@lavoie.sl>
2014-04-07 15:34:19 -0400

    added travis config

--- tests/MysqlTest.php
+++ tests/MysqlTest.php
@@ -7,7 +7,12 @@ class MysqlTest extends DoctrineCacheTest
     protected function getConnection()
     {
         $config = new \Doctrine\DBAL\Configuration();
-        $params = array('host' => 'localhost', 'user' => 'test', 'password' => 'test', 'dbname' => 'test', 'driver' => 'pdo_mysql');
+        if (getenv('TRAVIS')) {
+            $params = array('host' => 'localhost', 'user' => 'travis', 'dbname' => 'dcacheprovider_test', 'driver' => 'pdo_mysql');
+        } else {
+            $params = array('host' => 'localhost', 'user' => 'test', 'password' => 'test', 'dbname' => 'test', 'driver' => 'pdo_mysql');
+        }
+
         $connection = \Doctrine\DBAL\DriverManager::getConnection($params, $config);
 
         return $connection;
